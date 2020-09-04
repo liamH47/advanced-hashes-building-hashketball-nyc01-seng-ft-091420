82aab7deda7d6babea05487469bf7c2551812295
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-04 19:57:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,7 +60,27 @@ def game_hash
      team_name: "Charlotte Hornets",
      colors: ["Turquoise", "Purple"],
      players: {
-       
+       "Jeff Adrien" => {
+         number: 4,
+         shoe: 18,
+         points: 10,
+         rebounds: 1,
+         assists: 1,
+         steals: 2,
+         blocks: 7,
+         slam_dunks: 2
+       },
+       "Bismack Biyombo" => {
+         number: 0,
+         shoe: 16,
+         points: 12,
+         rebounds: 4,
+         assists: 7,
+         steals: 22,
+         blocks: 15,
+         slam_dunks: 10
+       },
+       ""
      }
    }
   }
