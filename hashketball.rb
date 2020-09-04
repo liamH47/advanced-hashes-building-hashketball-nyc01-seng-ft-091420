ef5066e1fa3bf577e1fe3f72b375592b044b2e9d
Liam Hanafee-Areces <liamhanafee@gmail.com>
2020-09-04 20:05:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 def game_hash
   {
    :home {
-     team_name: "Brooklyn Nets",
-     colors: ["Black", "White"],
+     team_name: "Brooklyn Nets"
+     colors: ["Black", "White"]
      players: {
        "Alan Anderson" => {
          number: 0,
@@ -57,8 +57,8 @@ def game_hash
      }
    } 
    :away {
-     team_name: "Charlotte Hornets",
-     colors: ["Turquoise", "Purple"],
+     team_name: "Charlotte Hornets"
+     colors: ["Turquoise", "Purple"]
      players: {
        "Jeff Adrien" => {
          number: 4,
